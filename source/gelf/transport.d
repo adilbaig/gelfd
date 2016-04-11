9a172f7e75c270a6d2f7064784c507f5ee0045e8
Adil Baig <adil.baig@aidezigns.com>
2016-04-12 01:32:31 +0530

    The new Chunks InputRange. W unittests

--- source/gelf/transport.d
+++ source/gelf/transport.d
@@ -9,11 +9,99 @@ public:
     enum MAX_CHUNKS = 128; //"A message MUST NOT consist of more than 128 chunks."
 
     /**
+     This struct converts a Message into chunks as defined in http://docs.graylog.org/en/latest/pages/gelf.html#chunked-gelf
+     
+     It is an InputRange.
+     */
+    struct Chunks {
+
+        import std.outbuffer : OutBuffer;
+        import std.range : Chunks;
+        import std.random : uniform;
+        
+        private :
+            Chunks!(const(ubyte)[]) byteChunks;
+            OutBuffer buffer;
+            ulong messageId;
+            byte sequenceNo;
+            ubyte total;
+        
+        public:
+        
+        /**
+         Convert a Message into chunks
+         
+         Params:
+             message   = The message to compress
+             chunkSize = The size of each chunk in bytes. Default : 8192
+             
+         Throws: MessageTooLargeException if # of chunks > 128.
+         */
+        this(Message message, uint chunkSize = 8192)
+        {
+            this(message.toBytes(), chunkSize);
+        }
+        
+        /**
+         Use this when you've converted a message to bytes. Use this after you have compressed your message
+         
+         Params:
+             message   = The compressed message.
+             chunkSize = The size of each chunk in bytes. Default : 8192 
+             messageId = The messageId. Do not use this, it is only used for unittests. Default : uniform(0, ulong.max) 
+             
+         Throws: MessageTooLargeException if # of chunks > 128.
+         */
+        this(const ubyte[] message, uint chunkSize = 8192, ulong messageId = uniform(0, ulong.max))
+        {
+            auto t = (message.length / (chunkSize - 12)) + 1;
+            if (t > MAX_CHUNKS) {
+                import std.conv : to;
+                throw new MessageTooLargeException("Message has "~ to!string(t) ~" chunks. Cannot be larger than " ~ MAX_CHUNKS ~ " chunks");
+            }
+            
+            this.messageId = messageId;
+            sequenceNo = 0;
+            
+            buffer = new OutBuffer();
+            buffer.reserve(chunkSize);
+
+            import std.range : chunks;    
+            byteChunks = chunks(message, chunkSize - 12);
+            total = cast(ubyte)byteChunks.length();
+        }
+        
+        ubyte[] front() {
+            buffer.offset = 0;
+            buffer.write(cast(ubyte)0x1e);
+            buffer.write(cast(ubyte)0x0f);
+            buffer.write(messageId);
+            buffer.write(sequenceNo);
+            buffer.write(total);
+            buffer.write(byteChunks[sequenceNo]);
+            
+            return buffer.toBytes();
+        }
+    
+        void popFront() {
+            sequenceNo++;
+        }
+        
+        bool empty() {
+            return !(sequenceNo < byteChunks.length());
+        }
+        
+        auto length() {
+            return byteChunks.length();
+        }
+    }
+    
+    /**
     This function provides a convenient way to send chunked GELF messages to Graylog.
     It automatically chunks a message based on $(D_PARAM packetSizeBytes).
 
     Params:
-         packetSizeBytes = The size of each chunk in bytes. Default : 81924
+         packetSizeBytes = The size of each chunk in bytes. Default : 8192
          compressed      = If true, compress the message using zlib. Default : false
 
     Throws: Exception if # of chunks > 128.
@@ -27,53 +115,51 @@ public:
     s.sendChunked(gelfMessage, 500);
     -------------------------
 
-    Returns: The number of packets sent
+    Returns: void
     */
     auto sendChunked(Socket socket, Message message, uint packetSizeBytes = 8192, bool compressed = false)
     {
         import std.zlib;
-        return chunkAndSend(socket, (compressed) ? compress(message.toString()) : cast(ubyte[])message.toString(), packetSizeBytes);
+        foreach(c; Chunks((compressed) ? compress(message.toString()) : message.toBytes(), 500))
+            socket.send(c);
     }
 
-private:
+    class MessageTooLargeException : Exception {
+        this(string msg) { super(msg); }
+    }
 
-    pragma(inline):
-    auto chunkAndSend(Socket socket, const(ubyte[]) message, uint packetSizeBytes)
-    {
-        import std.random : uniform;
-        import std.outbuffer : OutBuffer;
-        import std.range : chunks;
+unittest {
     
-        auto msgLength = message.length;
-        if(msgLength < packetSizeBytes) {
-            socket.send(message);
-            return 1;
-        }
+    auto m = Message("localhost","An alert message");
+//    import std.stdio;
+//    writeln(m.toBytes, m.toBytes().length);
 
-        auto t = (msgLength / packetSizeBytes) + 1;
-        if (t > MAX_CHUNKS) {
-            throw new Exception("Message too large");
-        }
-
-        ubyte total = cast(ubyte)t;
-        ulong messageId = uniform(0, long.max);
-
-        auto buffer = new OutBuffer();
-        buffer.reserve(packetSizeBytes);
-
-        byte sequenceNo = 0;
-        auto chks = chunks(message, packetSizeBytes - 12);
-        foreach(c; chks) {
-            buffer.offset = 0;
-            buffer.write(cast(ubyte)0x1e);
-            buffer.write(cast(ubyte)0x0f);
-            buffer.write(messageId);
-            buffer.write(sequenceNo++);
-            buffer.write(total);
-            buffer.write(c);
-
-            socket.send(buffer.toBytes());
-        }
-
-        return total;
+    import std.random : uniform;
+    auto mid = uniform(0, ulong.max);
+    auto chunks = Chunks(m.toBytes, 20, mid);
+    foreach(c; chunks) {
+//        writeln(c);
+        assert(c[0 .. 2] == [0x1e, 0x0f]); //Magic bytes
+        
+        import std.bitmanip : littleEndianToNative;
+        
+        ubyte[8] bytes = c[2 .. 10];
+        assert(littleEndianToNative!ulong(bytes) == mid); // 8byte message id
+        
+        assert(c[11] == (m.toBytes().length / (20 - 12)) + 1); //1 byte - Total number of chunks this message has
     }
+    
+    import std.zlib : compress;
+    
+    auto compressed = compress(m.toBytes);
+    chunks = Chunks(compressed, 20, mid);
+    foreach(c; chunks) {
+        assert(c[11] == (compressed.length / (20 - 12)) + 1); //1 byte - Total number of chunks this message has
+    }
+    
+    // Check if message is smaller than chunk length, then it makes one chunk only
+    chunks = Chunks(m.toBytes, cast(uint)(m.toBytes.length + 12), mid);
+    foreach(c; chunks) {
+        assert(c[11] == 1); //Only 1 chunk
+    }
+}
